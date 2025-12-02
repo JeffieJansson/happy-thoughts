b4b1842048a58331cd4bf5338201cbbb8f216373
Jennifer Jansson <jenniferjansson92@gmail.com>
2025-12-02 19:08:16 +0100

    add time posted in hours and minutes

--- src/components/ThoughtCard.jsx
+++ src/components/ThoughtCard.jsx
@@ -3,7 +3,8 @@ export const ThoughtCard = ({ thought }) => {
     <article className="thought-card">
       <p className="thought-message">{thought?.message}</p>
       <div className="thought-footer">
-        <span className="time">{thought?.createdAt || ''}</span>
+          {/* shows time posted thought*/}
+        <span className="time">{new Date(thought.createdAt).toLocaleTimeString()}</span>
       </div>
     </article>
   )
