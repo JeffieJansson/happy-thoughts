efc35dca9c40739c76c96f7cb697d6d4794e79d5
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-02-01 20:43:39 +0100

    Added edit and delete to ThoughtCard uwith useState, event handlers, and save/cancel buttons

--- src/components/ThoughtCard.jsx
+++ src/components/ThoughtCard.jsx
@@ -2,19 +2,71 @@ import { useState } from 'react'
 import ReactTimeAgo from 'react-timeago'
 import '../styles/thoughtCard.css'
 
-export const ThoughtCard = ({ thought, onLike }) => {
+export const ThoughtCard = ({ thought, onLike, onEdit, onDelete }) => {
 
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedMessage, setEditedMessage] = useState(thought.message)
+  const [deleted, setDeleted] = useState(false)
   const [hasLiked, setHasLiked] = useState(false)
+
+
   const handleLike = () => {
     if (hasLiked) return 
     setHasLiked(true) 
     if (onLike) onLike(thought._id) 
   }
 
+  if (deleted) return null;
+
+
+
   return (
     <article className="thought">
+     {isEditing ? (
+      <div>
+        <input
+          value={editedMessage}
+          onChange={e => setEditedMessage(e.target.value)}
+        />
+        <button 
+          className="save-button" 
+          onClick={() => {
+          onEdit(thought._id, editedMessage);
+          setIsEditing(false);
+        }}>
+          Save
+        </button>
+        <button className="cancel-button" onClick={() => setIsEditing(false)}>Cancel</button>
+      </div>
+    ) : (
       <p className="message">{thought.message}</p>
+    )}
+
+
       <div className="meta"> 
+        <div className="thought-actions">
+
+        <button
+          className="edit-button"
+          onClick={() => setIsEditing(true)}
+          aria-label="Edit this thought"
+        >
+          ✏️
+        </button>
+
+        <button
+          className="delete-button"
+          onClick={() => {
+            onDelete(thought._id)
+            setDeleted(true)
+          }}
+          aria-label="Delete this thought"
+        >
+          🗑️
+
+        </button>
+
+        </div>
         <button
           className={`like-button ${hasLiked ? 'liked' : ''}`}
           onClick={handleLike}
